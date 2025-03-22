04a40f6ae749494c892777ceea1e7b5510cc2835
Enzo2198 <gianggvf8209@fullstack.edu.vn>
2025-03-22 15:04:06 +0700

    Update homeworks

---
+++ Homeworks-JS/Day-07/Exercise-01.js
@@ -0,0 +1,47 @@
+const cart = [
+    { id: 1, name: "Laptop", price: 1500 },
+    { id: 2, name: "Phone", price: 800 },
+    { id: 3, name: "Tablet", price: 500 }
+];
+
+/*
+
+   initialize function removeItemAfterDelay (productId, delay, cb)
+
+                     │
+                     │
+                     ▼
+    find index by const indexItem = findIndex
+
+                     │
+                     │
+                     ▼          no      ┌───────────────────┐
+          if indexItem !== -1  ───────► │log 'not found id' │
+                                        └───────────────────┘
+                    │
+                    │ yes
+                    ▼
+      ┌─────────────────────────────────────────┐
+      │setTimeout cart.splice (indexItem, 1)    │
+      │                                         │
+      │call back cart after remove item same id │
+      │                                         │
+      └─────────────────────────────────────────┘
+
+ */
+const removeItemAfterDelay = (productId, delay, cb) => {
+    const indexItem = cart.findIndex(item => item.id === productId)
+
+    if (indexItem !== -1) {
+        setTimeout(() => {
+            cart.splice(indexItem, 1);
+            cb(cart)
+        }, delay);
+    } else {
+        console.log('Not found id')
+    }
+}
+
+removeItemAfterDelay(2, 1000, (cartUpdatedItem) => {
+    console.log('Update Cart: ', cartUpdatedItem);
+})
